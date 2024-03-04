05284eda09dabfcb6e3fb9bba02ff92d1d4cf6c6
Devang Vishnu <thedevangvishnu@gmail.com>
2024-03-04 17:17:52 +0530

    Add Login page and change the styling of Register
    Add Login page which will have similar structure
    and styling to the Register page.
    
    Add new background images into assests and use these
    new images as background images for both the pages.
    
    Add the login into App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Layout>Home</Layout>} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,30 +1,26 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useForm, SubmitHandler } from "react-hook-form";
 
-import RegisterImg from "../assets/bg1.png";
+import RegisterImg from "../assets/bg2.png";
 import { SiPlanetscale } from "react-icons/si";
-import { FaUser } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
 import { FaLock } from "react-icons/fa";
 
-type RegisterFormType = {
-  name: string;
+type LoginFormType = {
   email: string;
   password: string;
-  confirmPassword: string;
 };
 
-const Register = () => {
+const Login = () => {
   const navigate = useNavigate();
 
   const {
     register,
-    watch,
     handleSubmit,
     formState: { errors },
-  } = useForm<RegisterFormType>();
+  } = useForm<LoginFormType>();
 
-  const onSubmit: SubmitHandler<RegisterFormType> = (data) => {
+  const onSubmit: SubmitHandler<LoginFormType> = (data) => {
     console.log(data);
   };
 
@@ -45,12 +41,12 @@ const Register = () => {
 
       {/* form */}
       <div className="w-full flex flex-col flex-1 items-center justify-center px-4 py-6 h-full bg-stone-100">
-        <div className="flex flex-col items-center mb-6">
-          <h2 className="text-3xl  mb-4 md:mb-6 text-center font-semibold text-black">
-            Create Account
+        <div className="flex flex-col items-center mb-10">
+          <h2 className="text-4xl mb-4 md:mb-8 text-center font-semibold text-black">
+            Login
           </h2>
-          <p className="text-center w-[90%] text-sm text-slate-600">
-            We are glad that you've decided to join the community!
+          <p className="text-center w-[80%] md:w-[70%] text-sm text-slate-600">
+            Welcome back! We missed you a lot. Sign in and let's get going.
           </p>
         </div>
 
@@ -59,30 +55,6 @@ const Register = () => {
           className="flex flex-col items-center py-6 px-4 gap-5"
           onSubmit={handleSubmit(onSubmit)}
         >
-          <div>
-            <div className="flex items-center border-b-2 border-stone-400 pb-1">
-              <label htmlFor="name" className="">
-                <FaUser className="text-stone-400" />
-              </label>
-              <input
-                id="name"
-                type="text"
-                placeholder="Enter full name"
-                className="w-[90%] outline-none bg-transparent border-none px-2 text-sm placeholder:text-sm"
-                {...register("name", { required: "This field is required!" })}
-              />
-            </div>
-
-            {/* error */}
-            <div className="h-[20px] mt-1">
-              {errors.name && (
-                <p className="text-xs text-red-600 font-medium italic">
-                  {errors.name.message}
-                </p>
-              )}
-            </div>
-          </div>
-
           <div>
             <div className="flex items-center border-b-2 border-stone-400 pb-1">
               <label htmlFor="email" className="">
@@ -137,50 +109,19 @@ const Register = () => {
             </div>
           </div>
 
-          <div>
-            <div className="flex items-center border-b-2 border-stone-400 pb-1">
-              <label htmlFor="confirmPassword" className="">
-                <FaLock className="text-stone-400" />
-              </label>
-              <input
-                id="confirmPassword"
-                type="password"
-                placeholder="Confirm password"
-                className="text-sm w-[90%] outline-none border-none bg-transparent px-2 placeholder:text-sm"
-                {...register("confirmPassword", {
-                  required: "This field is required!",
-                  validate: (value) => {
-                    if (watch("password") != value) {
-                      return "Password does not match!";
-                    }
-                  },
-                })}
-              />
-            </div>
-
-            {/* error */}
-            <div className="h-[20px] mt-1 ">
-              {errors.confirmPassword && (
-                <p className="text-xs text-red-600 font-medium italic">
-                  {errors.confirmPassword.message}
-                </p>
-              )}
-            </div>
-          </div>
-
           <div className="flex items-center w-full mt-4">
             <button className="w-full py-3 bg-black rounded-[3rem] text-white uppercase font-semibold tracking-wider transition-[background-color] duration-300 hover:bg-stone-900">
-              Sign up
+              Sign in
             </button>
           </div>
 
           <p className="text-sm text-slate-700 text-center">
-            Already have an account?{" "}
+            Don't have an account?{" "}
             <Link
               to="/login"
               className="font-semibold transition-[color] duration-300 text-black hover:text-stone-900"
             >
-              Log in
+              Sign up
             </Link>
           </p>
         </form>
@@ -189,4 +130,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Login;
